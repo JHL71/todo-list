a1911ca04098778f289d198be838e84c8ddc0bdd
JHL71 <neko0717@naver.com>
2023-10-04 22:42:45 +0900

    edit css

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -11,18 +11,33 @@ const Wrap = styled.div`
   grid-column: 2 / 3;
   width: 100%;
   height: 100%;
+  overflow: hidden;
+  border: solid black 1px;
+  border-radius: 15px;
+  background-color: #74b9ff;
 `
 
 const Title = styled.h1`
+  padding-top: 5px;
   font-size: 24px;
   font-weight: bold;
+  text-align: center;
+`
+
+const PaddingLeft = styled.div`
+  padding-left: 10px;
 `
 
 const SubTitle = styled.h2`
   font-size: 16px;
   font-weight: bold;
+  padding-left: 10px;
 `
 
+const Ul = styled.ul`
+  height: 50%;
+  overflow: scroll;
+`
 
 const ToDoList = () => {
   const toDos = useRecoilValue(toDoSelector);  
@@ -37,21 +52,27 @@ const ToDoList = () => {
     <Wrap>
       <Title>Todos</Title>
       <hr />
-      <h2>Add Category</h2>
-      <CreateCategory />
+      <PaddingLeft>
+        <h2>Add Category</h2>
+        <CreateCategory />
+      </PaddingLeft>
       <hr />
-      <select onInput={onInput} defaultValue={"To Do"}>
-        {categories?.map((category) => {
-          return (
-            <option key={category+'i'} value={category}>{category}</option>
-          )
-        })}
-      </select>
-      <CreateToDo />
+      <PaddingLeft>
+        <select onInput={onInput} defaultValue={"To Do"}>
+          {categories?.map((category) => {
+            return (
+              <option key={category+'i'} value={category}>{category}</option>
+            )
+          })}
+        </select>
+        <CreateToDo />
+      </PaddingLeft>
       <hr />
       <SubTitle>{category}</SubTitle>
       <br></br>
-      {toDos?.map((toDo) => <ToDo key={toDo.id} {...toDo} />)}
+      <Ul>
+        {toDos?.map((toDo) => <ToDo key={toDo.id} {...toDo} />)}
+      </Ul>
     </Wrap>
   )
 }
